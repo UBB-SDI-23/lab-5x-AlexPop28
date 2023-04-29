3fb215c11d3451d8a2acde2252667b32a64a29d5
Alex Pop <popalexnicolae@gmail.com>
2023-04-29 13:53:47 +0300

    Add urls to TokenObtainPairView and TokenRefreshView

--- movieswebapp/moviesapp/urls.py
+++ movieswebapp/moviesapp/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from movieswebapp.moviesapp import views
 
@@ -50,4 +51,6 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + router.urls

1969632787d024e5fdd40db57e553029df1df17a
Alex Pop <popalexnicolae@gmail.com>
2023-05-10 11:42:04 +0300

    Run sql scripts in a new process

--- movieswebapp/moviesapp/utils.py
+++ movieswebapp/moviesapp/utils.py
@@ -1,8 +1,13 @@
 import os
+import subprocess
 
 
 def run_sql_script(path: str) -> None:
     os.environ["PGPASSWORD"] = os.environ["DB_PASSWORD"]
-    command = os.environ["DB_CLIENT_CONNECTION_STRING"] % (path,)
-    os.system(command)
+    sql_command = os.environ["DB_CLIENT_CONNECTION_STRING"] % (path,)
+    post_command = os.environ["DB_CLIENT_CONNECTION_STRING"] % (
+        "sql_scripts/post_gen.sql",
+    )
+    command = f"{sql_command} && {post_command}"
+    subprocess.Popen(command, shell=True)
     print(command)
